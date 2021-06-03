7d37987bfcbe42186149b9eedbe9bc4c0a613ccc
Carlos Arturo Ramirez <eramirezcarlos@gmail.com>
2021-06-02 21:23:05 -0400

    Adding styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import  { Fragment } from "react";
-//import logo from './logo.svg';
+import './assets/css/bootstrap.min.css';
+import './assets/css/font-awesome.min.css';
+import './assets/css/magnific-popup.css';
+//import './assets/css/owl.carousel.css';
+import './assets/css/animate.min.css';
+import './assets/css/responsive.css';
+import './assets/theme-color/color_1.css';
+
 import './App.css';
+
 import Layout from "./containers/Layout";
 
 
